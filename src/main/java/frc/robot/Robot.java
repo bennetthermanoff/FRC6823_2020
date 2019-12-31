1bf75aaad2ead4a43d24d7e52544e04107d71933
Bennett Hermanoff <bennetthermanoff@gmail.com>
2019-12-30 14:23:14 -1000

    Removed Tank Drive Code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -12,10 +12,7 @@ import edu.wpi.first.wpilibj.PWMVictorSPX;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
-/**
- * This is a demo program showing the use of the RobotDrive class, specifically
- * it contains the code necessary to operate a robot with tank drive.
- */
+
 public class Robot extends TimedRobot {
   
   private Joystick joystick = new Joystick (0); //creates new joystick
