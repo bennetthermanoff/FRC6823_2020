8becfbe1e19fa5ddb336db7a8c75bbe54fe3f0f1
6823Code <usmrobotics@ga.usmk12.org>
2020-01-09 17:23:44 -0600

    Make things finally work with 2020 libs
    Co-authored-by: Sword <quinn@bigbrother.group>

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,14 +7,21 @@
 //hey look it's some code! Incredible
 package frc.robot;
 
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+//import com.revrobotics.CANSparkMax;
+//import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.Preferences;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.I2C.Port;
+
 import edu.wpi.first.wpilibj.util.Color;
 
 import com.revrobotics.ColorSensorV3; // Color sensor
@@ -26,24 +33,38 @@ import com.revrobotics.ColorSensorV3; // Color sensor
 public class Robot extends TimedRobot {
   private DifferentialDrive driveTrain;
   private Joystick driveStick;
-  private CANSparkMax left1CAN, left2CAN, right1CAN, right2CAN;
+  private Talon left, right;
+ // private CANSparkMax left1CAN, left2CAN, right1CAN, right2CAN;
   private double speedRate, turnRate;
-  private ColorSensorV3 colorSensor;
   private I2C.Port i2cPort = I2C.Port.kOnboard;
+  private ColorSensorV3 colorSensor = new ColorSensorV3(i2cPort);
+  private NetworkTableEntry tx, ty, ta, ts;
+  private NetworkTable table;
+  private Preferences prefs;
+
+
 
   @Override
   public void robotInit() {
-    left1CAN  = new CANSparkMax(1, MotorType.kBrushless);
-    left2CAN  = new CANSparkMax(2, MotorType.kBrushless);
-    right1CAN = new CANSparkMax(3, MotorType.kBrushless);
-    right2CAN = new CANSparkMax(4, MotorType.kBrushless);   
-    /**  The numbers 1-4 are the CAN id's for the SparkMAX's, configure their ID's via 
-     *   plugging the SparkMAX via USB and using their software.**/
+    left = new Talon(1);
 
-    SpeedControllerGroup driveTrainLeft  = new SpeedControllerGroup(left1CAN,  left2CAN);
-    SpeedControllerGroup driveTrainRight = new SpeedControllerGroup(right1CAN, right2CAN); //groups motors into one virtual ESC for each side.
+    right = new Talon(0);
 
-    driveTrain = new DifferentialDrive(driveTrainLeft, driveTrainRight);
+    /**
+     * The numbers 1-4 are the CAN id's for the SparkMAX's, configure their ID's via
+     * plugging the SparkMAX via USB and using their software.
+     **/
+
+    driveTrain = new DifferentialDrive(left, right);
+    driveStick = new Joystick(0);
+    prefs = Preferences.getInstance();
+    // setsup limelight table values
+
+    table = NetworkTableInstance.getDefault().getTable("limelight");
+    tx = table.getEntry("tx");
+    ty = table.getEntry("ty");
+    ta = table.getEntry("ta");
+    ts = table.getEntry("ts");
   }
   @Override
   public void teleopPeriodic() {
