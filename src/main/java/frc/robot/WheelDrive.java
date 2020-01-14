53093e5bed70f0091cdb539691a6dc59b77b3083
Bennett Hermanoff <bennetthermanoff@gmail.com>
2020-01-14 13:35:42 -0600

    Added Comments/Formatting

--- src/main/java/frc/robot/WheelDrive.java
+++ src/main/java/frc/robot/WheelDrive.java
@@ -1,33 +1,35 @@
 package frc.robot;
 
 import com.revrobotics.CANSparkMax;
-
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-
 import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.controller.PIDController;
 
 public class WheelDrive {
-    private final double MAX_VOLTS = 4.95; // CHANGE THIS VOLTAGE I HAVE NO IDEA WHAT OUR KIT USES.
-
+    private final double MAX_VOLTS = 4.95; // Voltage for the Andymark Absolute Encoders used in the SDS kit.
     private CANSparkMax angleMotor;
     private CANSparkMax speedMotor;
     private PIDController pidController;
-
     private AnalogInput angleEncoder;
     private double encoderOffset;
 
     public WheelDrive(int angleMotor, int speedMotor, AnalogInput angleEncoder, double encoderOffset) {
         this.angleMotor = new CANSparkMax(angleMotor, MotorType.kBrushless);
-        this.speedMotor = new CANSparkMax(speedMotor, MotorType.kBrushless);
+        this.speedMotor = new CANSparkMax(speedMotor, MotorType.kBrushless); // We're using CANSparkMax controllers, but
+                                                                             // not their encoders.
         this.angleEncoder = angleEncoder;
         this.encoderOffset = encoderOffset;
 
         // pidController = new PIDController(1, 0, 0, new AnalogInput(encoder),
         // this.angleMotor);
-        pidController = new PIDController(.5, 0, 0);
-        // pidController.setTolerance(20);
-        pidController.enableContinuousInput(0, MAX_VOLTS);
+        pidController = new PIDController(.5, 0, 0); // This is the PID constant, we're not using any
+                                                     // Integral/Derivative control but changing the P value will make
+                                                     // the motors more aggressive to changing to angles.
+
+        // pidController.setTolerance(20); //sets tolerance, shouldn't be needed.
+
+        pidController.enableContinuousInput(0, MAX_VOLTS); // This makes the PID controller understand the fact that for
+                                                           // our setup, 4.95V is the same as 0 since the wheel loops.
 
     }
 
@@ -49,10 +51,16 @@ public class WheelDrive {
         } // converts angle into the same scale that the encoder uses.
 
         pidController.setSetpoint(setpoint); // sets setpoint from PID controller
-        double boundedOffset = (angleEncoder.getVoltage() + encoderOffset) % MAX_VOLTS;
+        double boundedOffset = (angleEncoder.getVoltage() + encoderOffset) % MAX_VOLTS; // Combines reading from encoder
+                                                                                        // with the encoderOffset that
+                                                                                        // can be changed in
+                                                                                        // SmartDashboard.
 
-        double pidOut = pidController.calculate(boundedOffset, setpoint);
-        angleMotor.set(-pidOut);
+        double pidOut = pidController.calculate(boundedOffset, setpoint);// calculates using PID the angle motor
+                                                                         // command.
+
+        angleMotor.set(-pidOut); // colten is god for figuring out our problem on 1/14/20 was this not being
+                                 // negative.
 
         Robot.prefs.putDouble("Encoder [" + angleEncoder.getChannel() + "] boundedOffset", boundedOffset);
         Robot.prefs.putDouble("Encoder [" + angleEncoder.getChannel() + "] setpoint", setpoint);
