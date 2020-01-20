fb8e08cc463a264c4e1935b4e0c2c54978098269
6823Code <usmrobotics@ga.usmk12.org>
2020-01-20 11:55:09 -0600

    now it works

--- src/main/java/frc/robot/ColorSensor.java
+++ src/main/java/frc/robot/ColorSensor.java
@@ -45,7 +45,7 @@ public class ColorSensor {
     pidcontroller.enableContinuousInput(0, 1); // So that the code knows it's a wheel we are dealing with
   }
 
-  public ColorSensor() {
+  public ColorSensor(PWMVictorSPX spinner) {
     i2cPort = I2C.Port.kOnboard;
     colorSensor = new ColorSensorV3(i2cPort);
     moving = false;
@@ -55,7 +55,7 @@ public class ColorSensor {
     colorSelection = 0;// this
     directionOfSpin = 0;
     pidcontroller = new PIDController(0.5, 0, 0); // changed from 0.2, 0, 0 to overshoot
-    spinner = new PWMVictorSPX(5); // this is the motor for the spinner
+    this.spinner = spinner; // this is the motor for the spinner
     pidcontroller.enableContinuousInput(0, 1); // So that the code knows it's a wheel we are dealing with
   }
 
@@ -75,6 +75,7 @@ public class ColorSensor {
       activateSpinner();
     }
     showOnDashBoard(detectedColor);
+
   }
 
   // returns true if the rgb of two colors is within the error value
@@ -160,9 +161,10 @@ public class ColorSensor {
 
   public void activateSpinner() {
     directionOfSpin = NextDistanceSpun();
-    if (!colorSelected().equals("unknown"))// if the color is unknown it won't set a direction and keep doing what its
+    if (!colorSelected().equals("unknown")){// if the color is unknown it won't set a direction and keep doing what its
                                            // doing
-      spinner.set(directionOfSpin);
+      spinner.set(directionOfSpin * 2);
+  }
     moving = directionOfSpin != 0;
   }
 }
